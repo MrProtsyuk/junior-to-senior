6649917083698001b9130d0fd67f5a6c116fe064
Your Name <markprotsyuk@Marks-MacBook-Pro-2.local>
2023-01-30 12:50:17 -0800

    added notes from extra bits

---
+++ ExtraBits/extra.js
@@ -0,0 +1,19 @@
+// Simplicity or complexity --1
+// Code should always be left with comments, correct spacing, and easy for another dev to come in and understand what is going on
+// Simple code is readable and bug free
+// You will always need to write complex code but you should try to simplify it the best you can
+
+// Npm is not your friend --2
+// Do you really need a library or can you build a tool on your own?
+// Make sure the things that you add to your project are worth it and meaningful
+// It is always easy to add but it is difficult to clean up
+
+// Learn to learn --3
+// Become efficient in learning skills
+// Figure out what is your best learning style and then learn everything in that style
+// To become a successful dev you need to get good at learning
+
+// Start with why --4
+// Should you dedicate your time to learning something brand new or should you spend a little extra time in the fundamentals
+// When learning new things ask yourself why should I learn this?
+// Be an observer and when you see a good foundation then you can go ahead and dedicate your time in it.
